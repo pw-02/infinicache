725a75cee080dc9530f22736330b21c8681c56c3
pw-02 <watters.patrick@outlook.com>
2025-03-20 13:58:26 -0700

    x1

--- proxy/server/config.go
+++ proxy/server/config.go
@@ -7,14 +7,14 @@ import (
 )
 
 const AWSRegion = "us-west-2"
-const LambdaMaxDeployments = 400
-const NumLambdaClusters = 400
+const LambdaMaxDeployments = 1
+const NumLambdaClusters = 1
 const LambdaStoreName = "LambdaStore" // replica version (no use)
-const LambdaPrefix = "CacheNode"
+const LambdaPrefix = "CacheNodeA"
 const InstanceWarmTimout = 1 * time.Minute
-const InstanceCapacity = 1536 * 1000000 // MB
-const InstanceOverhead = 100 * 1000000  // MB
-const ServerPublicIp = ""               // Leave it empty if using VPC.
+const InstanceCapacity = 128 * 1000000 // MB
+const InstanceOverhead = 100 * 1000000 // MB
+const ServerPublicIp = ""              // Leave it empty if using VPC.
 
 func init() {
 	lambdastore.WarmTimout = InstanceWarmTimout
